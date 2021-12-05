52f6f5de461ec603fa5b98fce7b019d0854a24fc
tarooooooo <ryutaromaeda83@gmail.com>
2021-12-05 22:20:34 +0900

    スッキリわかるJava_2

---
+++ src/java1/Java1_3.java
@@ -0,0 +1,9 @@
+package java1;
+
+public class Java1_3 {
+  public static void main(String[] args) {
+	  String s1 = "Java programming";
+	  System.out.println("文字列s1の4文字目以降は" + s1.substring(3));
+	  System.out.println("文字列s1の4〜8文字目は" + s1.substring(3, 8));
+  }
+}
